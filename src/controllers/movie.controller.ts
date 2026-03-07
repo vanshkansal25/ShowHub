39bafb99af30ee56c9c20f903187349d3863fa15
@vanshkansal25 <kansalv578@gmail.com>
2026-03-07 15:43:04 +0530

    feat: Implemented getMoviesByCity and getMoviesBySlug controller

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -1,133 +1,301 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { Movie } from "../models/movies.model";
 import { ApiError } from "../utils/apiError";
 import { ApiResponse } from "../utils/apiResponse";
 import slugify from "slugify";
+import { Show } from "../models/shows.model";
 
 // ADMIN FUNCTIONS
 
 //addMovie
-export const addMovie = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
-    const {title,description,duration,language,genre,releaseDate,posterUrl} = req.body;
-   //TODO: ADD CLOUDINARY FOR POSTER UPLOAD
-   if(!title || !description || !duration || !language || !genre || !releaseDate){
-    throw new ApiError(400,"All fields are required");
-   }
-    let slug = slugify(title,{ lower: true, strict: true });
+export const addMovie = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const {
+            title,
+            description,
+            duration,
+            language,
+            genre,
+            releaseDate,
+            posterUrl,
+        } = req.body;
+        //TODO: ADD CLOUDINARY FOR POSTER UPLOAD
+        if (
+            !title ||
+            !description ||
+            !duration ||
+            !language ||
+            !genre ||
+            !releaseDate
+        ) {
+            throw new ApiError(400, "All fields are required");
+        }
+        let slug = slugify(title, { lower: true, strict: true });
 
-    // Ensure slug is unique
-    const existingSlug = await Movie.findOne({ slug });
-    if(existingSlug){
-        slug = `${slug}-${Math.floor(Math.random() * 10000)}`;
+        // Ensure slug is unique
+        const existingSlug = await Movie.findOne({ slug });
+        if (existingSlug) {
+            slug = `${slug}-${Math.floor(Math.random() * 10000)}`;
+        }
+        const movie = await Movie.create({
+            title,
+            slug,
+            description,
+            duration,
+            language,
+            genre,
+            releaseDate: new Date(releaseDate),
+            posterUrl,
+        });
+        res
+            .status(201)
+            .json(new ApiResponse(201, movie, "Movie added successfully"));
     }
-    const movie = await Movie.create({
-        title,
-        slug,
-        description,
-        duration,
-        language,
-        genre,
-        releaseDate:new Date(releaseDate),
-        posterUrl
-    })
-    res.status(201).json(new ApiResponse(201,movie,"Movie added successfully"));
-})
+);
 
 // update movie
-export const updateMovie = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
-    const {id} = req.params;
-    const {title,description,duration,language,genre,releaseDate} = req.body;
-    const movie = await Movie.findById(id);
-    if(!movie || movie.isDeleted){
-        throw new ApiError(404,"Movie not found");
+export const updateMovie = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+        const { title, description, duration, language, genre, releaseDate } =
+            req.body;
+        const movie = await Movie.findById(id);
+        if (!movie || movie.isDeleted) {
+            throw new ApiError(404, "Movie not found");
+        }
+        if (title && title !== movie.title) {
+            let slug = slugify(title, { lower: true, strict: true });
+        }
+        const updatedMovie = await Movie.findByIdAndUpdate(
+            id,
+            {
+                $set: {
+                    title: title || movie.title,
+                    description: description || movie.description,
+                    duration: duration || movie.duration,
+                    language: language || movie.language,
+                    genre: genre || movie.genre,
+                    releaseDate: releaseDate || movie.releaseDate,
+                },
+            },
+            {
+                new: true, // Return the modified document
+                runValidators: true, // Ensure Mongoose schema validation runs
+            },
+        ).select("-isDeleted");
+        res
+            .status(200)
+            .json(new ApiResponse(200, updatedMovie, "Movie updated successfully"));
     }
-    if(title && title !== movie.title){
-        let slug = slugify(title,{ lower: true, strict: true });
-    }
-    const updatedMovie = await Movie.findByIdAndUpdate(id,{
-        $set:{
-            title:title || movie.title,
-            description:description || movie.description,
-            duration:duration || movie.duration,
-            language:language || movie.language,
-            genre:genre || movie.genre,
-            releaseDate:releaseDate || movie.releaseDate
-        },
-        },{
-            new:true,// Return the modified document
-            runValidators:true,// Ensure Mongoose schema validation runs
-        }).select("-isDeleted");
-    res.status(200).json(new ApiResponse(200,updatedMovie,"Movie updated successfully"));
-})
+);
 // soft delete movie
 
-export const deleteMovie = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
-    const {id} = req.params;
-    const movie = await Movie.findByIdAndUpdate(id,
-        {isDeleted:true},
-        {new :true}
-    )
-    if(!movie){
-        throw new ApiError(404, "Movie not found");
+export const deleteMovie = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+        const movie = await Movie.findByIdAndUpdate(
+            id,
+            { isDeleted: true },
+            { new: true },
+        );
+        if (!movie) {
+            throw new ApiError(404, "Movie not found");
+        }
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "Movie soft-Deleted successfully"));
     }
-    return res.status(200).json(new ApiResponse(200,{},"Movie soft-Deleted successfully"))
-})
+);
 // update Poster
-
-
-
-
-
 
 // User Facing Functions
 
 //getAllMovies
-export const getAllMovies = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
-    const {page = 1 , limit = 10 , genre , language , search,upcoming = "false"} = req.query;
-    // building the filter object 
-    const filter :any = {isDeleted:false}
-    if(genre){
-        filter.genre = {$in:[genre]};
-    }
-    if(language){
-        filter.language = {$in:[language]};
-    }
-    if(search){
-        filter.$text = {$search:search} // MongoDB will return documents where searchedValue appears in indexed text fields.
-    }
-    if(upcoming === "true"){
-        filter.releaseDate = {$gt:new Date()};
-    }
+export const getAllMovies = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const {
+            page = 1,
+            limit = 10,
+            genre,
+            language,
+            search,
+            upcoming = "false",
+        } = req.query;
+        // building the filter object
+        const filter: any = { isDeleted: false };
+        if (genre) {
+            filter.genre = { $in: [genre] };
+        }
+        if (language) {
+            filter.language = { $in: [language] };
+        }
+        if (search) {
+            filter.$text = { $search: search }; // MongoDB will return documents where searchedValue appears in indexed text fields.
+        }
+        if (upcoming === "true") {
+            filter.releaseDate = { $gt: new Date() };
+        }
 
-    // pagination based query 
-    const skip = (Number(page)-1)*Number(limit);
+        // pagination based query
+        const skip = (Number(page) - 1) * Number(limit);
 
-    // because of pagination i require two thing movies,totalMovies so instead of running two db queries I will use promises to run this task simultaneously 
-    const [movies,totalMovies] = await Promise.all([
-        Movie.find(filter).sort(search ? { score: { $meta: "textScore" } } : "-releaseDate").skip(skip).limit(Number(limit)).select("title slug posterUrl genre language releaseDate"),
-        Movie.countDocuments(filter)
-    ])
+        // because of pagination i require two thing movies,totalMovies so instead of running two db queries I will use promises to run this task simultaneously
+        const [movies, totalMovies] = await Promise.all([
+            Movie.find(filter)
+                .sort(search ? { score: { $meta: "textScore" } } : "-releaseDate")
+                .skip(skip)
+                .limit(Number(limit))
+                .select("title slug posterUrl genre language releaseDate"),
+            Movie.countDocuments(filter),
+        ]);
 
-    // total pages 
+        // total pages
 
-    const totalPages = Math.ceil(totalMovies/Number(limit));
-    return res.status(200).json(
-        new ApiResponse(200, {
-            movies,
-            // pagination metadata for frontend
-            pagination: {
-                totalMovies,
-                totalPages,
-                currentPage: Number(page),
-                hasNextPage: Number(page) < totalPages
-            }
-        }, "Movies fetched successfully")
-    );
-    //TODO: ADD REDIS IMPLEMENTATION
-})
+        const totalPages = Math.ceil(totalMovies / Number(limit));
+        return res.status(200).json(
+            new ApiResponse(
+                200,
+                {
+                    movies,
+                    // pagination metadata for frontend
+                    pagination: {
+                        totalMovies,
+                        totalPages,
+                        currentPage: Number(page),
+                        hasNextPage: Number(page) < totalPages,
+                    },
+                },
+                "Movies fetched successfully",
+            ),
+        );
+        //TODO: ADD REDIS IMPLEMENTATION
+    }
+);
 //getMoviesByCity
+export const getMoviesByCity = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { city } = req.params;
+        if (!city || typeof city !== "string") {
+            throw new ApiError(400, "City name is required to fetch movies");
+        }
+        const moviesInCity = await Show.aggregate([
+            // 1. Join with Venue to filter by City
+            {
+                $lookup: {
+                    from: "venues",
+                    localField: "venueId",
+                    foreignField: "_id",
+                    as: "venueDetails",
+                },
+            },
+            // 2. Unwind the venueDetails array (converts it to an object)
+            { $unwind: "$venueDetails" },
+            // 3. Filter by City and Active Status
+            {
+                $match: {
+                    "venueDetails.city": { $regex: new RegExp(city, "i") }, // Case-insensitive match
+                    status: "Active",
+                    startTime: { $gte: new Date() }, // Only shows that haven't started yet
+                },
+            },
+            // 4. Join with Movie collection to get metadata
+            {
+                $lookup: {
+                    from: "movies",
+                    localField: "movieId",
+                    foreignField: "_id",
+                    as: "moviesDetail",
+                },
+            },
+            {
+                $unwind: "$moviesDetail",
+            },
+            // 5. Group by Movie ID so we don't get duplicates
+            {
+                $group: {
+                    _id: "$movieId", //grouping based on movie id movies with same id grouped together
+                    title: { $first: "$movieDetails.title" }, // $first returns the first value encountered in the group. Because all shows of the same movie have the same title.
+                    slug: { $first: "$movieDetails.slug" },
+                    posterUrl: { $first: "$movieDetails.posterUrl" },
+                    genre: { $first: "$movieDetails.genre" },
+                    language: { $first: "$movieDetails.language" },
+                    rating: { $first: "$movieDetails.rating" },
+                    // Also counting how many theaters are playing this
+                    totalTheaters: { $addToSet: "$venueId" },
+                },
+            },
+            // 6. Clean up the output
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    slug: 1,
+                    posterUrl: 1,
+                    genre: 1,
+                    language: 1,
+                    rating: 1,
+                    theaterCount: { $size: "$totalTheaters" },
+                },
+            },
+            // 7. Sort by Rating or Title
+            { $sort: { rating: -1 } },
+        ]);
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    moviesInCity,
+                    `Movies currently showing in ${city}`,
+                ),
+            );
+    }
+);
 //getMovieBySlug
+export const getMovieBySlug = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { slug } = req.params;
+        const movie = await Movie.findOne({
+            slug,
+            isDeleted: false,
+        });
+        if (!movie) {
+            throw new ApiError(404, "Movie not found or has been removed");
+        }
+        // 2. Check for Active Shows (Optimization)
+        // I don't need the full show data here, just a boolean "Is it playing?"
+        const hasActiveShows = await Show.exists({
+            movieId: movie._id,
+            status: "Active",
+            startTime: { $gt: new Date() },
+        });
+        // exists() returns either an object or null.
+
+        // 3. Response Structure
+        // Combine the movie data with the 'isBookable' status
+        const movieData = {
+            ...movie.toObject(),
+            isBookable: !!hasActiveShows, // Converts null/object to true/false
+        };
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, movieData, "Movie details fetched successfully"),
+            );
+    }
+);
 //searchMovies
+export const searchMovies = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        
+     },
+);
 //getComingSoon
+export const getComingSoon = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => { },
+);
 //getNowShowing
+export const getNowShowing = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => { },
+);
