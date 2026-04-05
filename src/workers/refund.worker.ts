dc4fb4b876d4980a2b57592abf0c6606e8ff3ee3
@vanshkansal25 <kansalv578@gmail.com>
2026-04-05 13:20:21 +0530

    Added logic to update wallet balance of user on refund

--- src/workers/refund.worker.ts
+++ src/workers/refund.worker.ts
@@ -2,21 +2,33 @@ import { Worker,Job } from "bullmq";
 import { razorpay } from "../utils/razorpay";
 import { Refund } from "../models/refund.model";
 import { redis } from "../utils/redis";
+import { User } from "../models/user.model";
 
-export const refundWorker = new Worker("refund_queue",async(job:Job)=>{
-    const {refundId,paymentTransactionId,amount} = job.data
-    const refundResponse = await razorpay.payments.refund(
-        paymentTransactionId,
-        {
-            amount:amount*100,
-            speed:"normal"
-        }
-    )
-    await Refund.findByIdAndUpdate(refundId,{
-        status:"PROCESSED",
-        refundTransactionId:refundResponse.id,
-        processedAt:new Date()
-    })
-},{
-    connection:redis
-})
+export const refundWorker = new Worker("refund_queue", async (job: Job) => {
+    const { refundId, paymentTransactionId, amount } = job.data;
+
+    try {
+        const refundResponse = await razorpay.payments.refund(paymentTransactionId, {
+            amount: amount * 100,
+            speed: "normal"
+        });
+        const refundRecord = await Refund.findById(refundId);
+        if (!refundRecord) throw new Error("Refund record not found");
+        const updatedUser = await User.findByIdAndUpdate(
+            refundRecord.userId,
+            { $inc: { walletBalance: amount } },
+            { new: true }
+        );
+        if (!updatedUser) throw new Error("User not found for wallet credit");
+        await Refund.findByIdAndUpdate(refundId, {
+            status: "PROCESSED",
+            refundTransactionId: refundResponse.id,
+            processedAt: new Date()
+        });
+        console.log(`Refund Success: ₹${amount} to ${updatedUser.userName}`);
+    } catch (error) {
+        console.error(`Refund Job ${job.id} failed:`, error);
+        await Refund.findByIdAndUpdate(refundId, { status: "FAILED" });
+        throw error; 
+    }
+}, { connection: redis });
